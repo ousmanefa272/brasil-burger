3015913a02d97cacd3cbd3f388579d2c53ccfa14
Ousmane FALL <ousmane.fall4@ism.edu.sn>
2025-12-23 17:23:45 +0100

    Créer et lister les commandes

---
+++ src/Controller/CommandeController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Commande;
+use App\Form\CommandeType;
+use App\Repository\CommandeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/commande')]
+final class CommandeController extends AbstractController
+{
+    #[Route('/', name: 'commande_index')]
+    public function index(CommandeRepository $repo): Response
+    {
+        return $this->render('commande/index.html.twig', [
+            'commandes' => $repo->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'commande_new')]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $commande = new Commande();
+        $commande->setDate(new \DateTime());
+
+        $form = $this->createForm(CommandeType::class, $commande);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($commande);
+            $em->flush();
+
+            return $this->redirectToRoute('commande_index');
+        }
+
+        return $this->render('commande/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
