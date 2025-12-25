a29f65349ef274733137945cf3c5692f7448bbf3
Ousmane FALL <ousmane.fall4@ism.edu.sn>
2025-12-26 00:40:32 +0100

    Créer un menu: ajout d'un menu via formulaire

--- src/Entity/Menu.php
+++ src/Entity/Menu.php
@@ -13,8 +13,40 @@ class Menu
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column]
+    private ?int $prix = null;
+
+    // ======================
+    // GETTERS & SETTERS
+    // ======================
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+        return $this;
+    }
+
+    public function getPrix(): ?int
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(int $prix): self
+    {
+        $this->prix = $prix;
+        return $this;
     }
 }
